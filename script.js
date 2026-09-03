3333ab397a3ddbbd2dfe9952c72883ec9efc5c98
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-03 10:35:55 +0200

    added another variable with different data

--- script.js
+++ script.js
@@ -7,7 +7,24 @@ const person = {
     petname: "Holger",
 }
 
+const andenPerson = {
+    firstname: "Simon",
+    lastname: "Andersen",
+    age: 33,
+    hobbies: ["Klatrer", "Gå ture", "Inkøb"],
+    hometown: "Kastrup",
+    married: true,
+    pet: {
+        type: "cat,",
+        name: "misser",
+    },
+};
+
 console.log(person.firstname)
 console.log(person.lastname)
 console.log(person["pet"])
 console.log(person["petname"]);
+
+andenPerson.hobbies.forEach(function(hobby) {
+    console.log(hobby)
+})
