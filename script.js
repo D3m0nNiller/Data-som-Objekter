cd410e91328211f21beea1e2fd929bc096ce544b
Nicklas Bregenborg Sivertsen <nicklas_sivertsen@hotmail.com>
2026-09-03 10:27:59 +0200

    added index and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+const person = {
+    firstname: "Ole",
+    lastname: "Erling",
+    age: 54,
+    hometown: "Roskilde",
+    pet: "Klapperslange",
+    petname: "Holger",
+}
+
+console.log(person.firstname)
+console.log(person.lastname)
+console.log(person["pet"])
+console.log(person["petname"]);
